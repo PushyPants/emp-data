d4972a6ddce4018e95902b961a2ce25e515529f0
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-06-13 19:08:16 -0500

    Connected to Firebase

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    // ============== Firebase ==============
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAtsKzX0nVjR7EX2V46N-wFq5lEikrjieY",
+        authDomain: "employeedatamanagement-86221.firebaseapp.com",
+        databaseURL: "https://employeedatamanagement-86221.firebaseio.com",
+        projectId: "employeedatamanagement-86221",
+        storageBucket: "employeedatamanagement-86221.appspot.com",
+        messagingSenderId: "170634068198"
+    };
+    firebase.initializeApp(config);
 
-
-
-
+    let database = firebase.database();
     
+
+
 });
